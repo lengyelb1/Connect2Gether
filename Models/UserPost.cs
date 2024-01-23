6af3c324f9cce67d017c8260b63ee45a7d0aa28c
Balázs Juhász <juhaszb@kkszki.hu>
2024-01-23 09:47:16 +0100

    Default api

---
+++ Models/UserPost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Connect2Gether_API.Models;
+
+public partial class UserPost
+{
+    public int Id { get; set; }
+
+    public int ImageId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public long Like { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual User User { get; set; } = null!;
+}
