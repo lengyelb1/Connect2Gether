615b04c6abac6951215a1bf653bc01f54bb51fe2
Balázs Juhász <juhaszb@kkszki.hu>
2024-01-23 11:56:48 +0100

    new comments

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using Connect2Gether_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Connect2Gether_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            using (var context = new Connect2getherContext())
+            {
+                try
+                {
+                    context.Comments.Add(comment);
+                    context.SaveChanges();
+                    return Ok("Sikeres feltöltés!");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
