a6d5f296081db1a73b87d99a6efeb8c690c133f8
juhaszb1 <juhaszb@kkszki.hu>
2024-02-26 19:28:37 +0100

    Authentication
    Hozzáadtuk az authentikációt a backendhez.

--- obj/Debug/net8.0/Connect2Gether_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Connect2Gether_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect2Gether_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf86b0f1213c04a5aa2fd8f7e38994fbc37947d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193e8f92a32998cc5e835c11cd9eb66ccc7bd09e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect2Gether_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect2Gether_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
