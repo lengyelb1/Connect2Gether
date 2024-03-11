8790ff539db012419ba57a88bb70a640cf8b0fd9
lbálint <lengyelb@kkszki.hu>
2024-03-11 10:56:16 +0100

    Git ignore

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Connect2Gether_API.Models;
+using Connect2Gether_API.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace Connect2Gether_API.Controllers
+{
+
+    /* Oldal funkciók */
+    /* Jelszó / azonosítóval való lekérés*/
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
---
+++ Models/UserPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Connect2Gether_API.Models;
+
+public partial class UserPost
+{
+    public int Id { get; set; }
+
+    public int? ImageId { get; set; } = null;
+
+    public string Description { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public long? Like { get; set; } = 0;
+
+    public int? UserId { get; set; }
+
+    public virtual ICollection<Comment>? Comments { get; set; } = null;
+
+    [JsonIgnore]
+    public virtual User? User { get; set; } = null!;
+}
