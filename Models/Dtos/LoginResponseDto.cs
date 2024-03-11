2afdbe3594dd3f152f0b7cdabf12d71f2707f9eb
lbálint <lengyelb@kkszki.hu>
2024-03-11 10:55:39 +0100

    Delete all to git ignore

--- Models/Dtos/LoginResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Connect2Gether_API.Models.Dtos
-{
-    public class LoginResponseDto
-    {
-        public UserDto User { get; set; }
-        public string? Token { get; set; }
-    }
-}
--- Models/Dtos/RegistrationRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Connect2Gether_API.Models.Dtos
-{
-    public class RegistrationRequestDto
-    {
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
-        public string? Email { get; set; }
-    }
-}
