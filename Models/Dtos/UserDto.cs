d058e8dcdf8a8c5132e05c9812f31489a4633cef
lbálint <lengyelb@kkszki.hu>
2024-03-11 10:48:58 +0100

    Delete al for git ignore

--- Models/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Connect2Gether_API.Models.Dtos
-{
-    public class UserDto
-    {
-        public int? Id { get; set; }
-        public string? UserName { get; set; }
-        public string? Email { get; set; }
-    }
-}
