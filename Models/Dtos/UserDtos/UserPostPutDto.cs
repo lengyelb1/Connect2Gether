04dd4730c68cd82c0819c0b86d3432d31927f463
lengyelb <lengyelb@kkszki.hu>
2024-03-18 18:47:50 +0100

    Delete all

--- Models/Dtos/UserDtos/UserPostPutDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Connect2Gether_API.Models.Dtos.UserDtos
-{
-    public class UserPostPutDto
-    {
-        public string Description { get; set; }
-        public string Title { get; set; }
-    }
-}
