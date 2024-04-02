2022d1e9efae96d9d514ee099aae7ec4854e12dd
juhaszb1 <juhaszb@kkszki.hu>
2024-04-02 11:43:01 +0200

    Transitioning from HTTP to HTTPS.

---
+++ Controllers/UserControllers/UserProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Connect2Gether_API.Models;
+using Connect2Gether_API.Models.Dtos.UserDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connect2Gether_API.Controllers.UserControllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        /*[HttpGet("UserProfile")]
+        public IActionResult UserProfile(int id, UserProfileDto userProfileDto)
+        {
+            using (var context = new Connect2getherContext())
+            {
+                try
+                {
+                    var user = context.Users.FirstOrDefault(x => x.Id == id);
+                    if (user == null) 
+                    {
+                        return StatusCode(404, "Nincs ilyen user");
+                    }
+                    else
+                    {
+                        user.Username = userProfileDto.UserName;
+                        user.Email = userProfileDto.Email;
+                        user.Point = userProfileDto.Points;
+                        user.RankId = userProfileDto.RankId;
+                        user.RegistrationDate = userProfileDto.RegistrationDate;
+                        context.UserPosts.Where(x => x.UserId == user.Id).Count();
+                    }    
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }*/
+    }
+}
---
+++ Models/Dtos/UserDtos/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Connect2Gether_API.Models.Dtos.UserDtos
+{
+    public class UserProfileDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int Points { get; set; }
+        public int RankId { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int PostDb { get; set; }
+        public int CommentDb { get; set; }
+    }
+}
