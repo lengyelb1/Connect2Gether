40e539f8611f11b6697d4e178a1ce9828525969a
Balázs Juhász <juhaszb@kkszki.hu>
2024-04-17 09:14:13 +0200

    Fixed Sql database

--- connect2gether.sql
+++ connect2gether.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Ápr 15. 10:12
+-- Létrehozás ideje: 2024. Ápr 17. 09:13
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -29,14 +29,12 @@ USE `connect2gether`;
 -- Tábla szerkezet ehhez a táblához `alertmessage`
 --
 
-CREATE TABLE IF NOT EXISTS `alertmessage` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `alertmessage` (
+  `Id` int(11) NOT NULL,
   `Title` varchar(128) NOT NULL,
   `UserId` int(11) NOT NULL,
-  `Description` varchar(128) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `UserId` (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `Description` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `alertmessage`
@@ -57,18 +55,21 @@ INSERT INTO `alertmessage` (`Id`, `Title`, `UserId`, `Description`) VALUES
 -- Tábla szerkezet ehhez a táblához `comment`
 --
 
-CREATE TABLE IF NOT EXISTS `comment` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comment` (
+  `Id` int(11) NOT NULL,
   `Text` text NOT NULL,
   `PostId` int(11) NOT NULL,
   `UserId` int(11) NOT NULL,
   `CommentId` int(11) NOT NULL,
-  `uploadDate` date DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `CommentId` (`CommentId`),
-  KEY `UserId` (`UserId`),
-  KEY `PostId` (`PostId`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `uploadDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `comment`
+--
+
+INSERT INTO `comment` (`Id`, `Text`, `PostId`, `UserId`, `CommentId`, `uploadDate`) VALUES
+(24, 'string', 31, 18, 0, '2024-04-16');
 
 -- --------------------------------------------------------
 
@@ -76,14 +77,13 @@ CREATE TABLE IF NOT EXISTS `comment` (
 -- Tábla szerkezet ehhez a táblához `images`
 --
 
-CREATE TABLE IF NOT EXISTS `images` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `images` (
+  `Id` int(11) NOT NULL,
   `Image1` mediumblob NOT NULL,
   `Image2` mediumblob NOT NULL,
   `Image3` mediumblob NOT NULL,
-  `Image4` mediumblob NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `Image4` mediumblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `images`
@@ -98,14 +98,11 @@ INSERT INTO `images` (`Id`, `Image1`, `Image2`, `Image3`, `Image4`) VALUES
 -- Tábla szerkezet ehhez a táblához `liked_posts`
 --
 
-CREATE TABLE IF NOT EXISTS `liked_posts` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `liked_posts` (
+  `Id` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
-  `PostID` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `UserID` (`UserID`),
-  KEY `PostID` (`PostID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `PostID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- A tábla adatainak kiíratása `liked_posts`
@@ -120,11 +117,10 @@ INSERT INTO `liked_posts` (`Id`, `UserID`, `PostID`) VALUES
 -- Tábla szerkezet ehhez a táblához `permissions`
 --
 
-CREATE TABLE IF NOT EXISTS `permissions` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(128) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+CREATE TABLE `permissions` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `permissions`
@@ -141,13 +137,12 @@ INSERT INTO `permissions` (`Id`, `Name`) VALUES
 -- Tábla szerkezet ehhez a táblához `ranks`
 --
 
-CREATE TABLE IF NOT EXISTS `ranks` (
+CREATE TABLE `ranks` (
   `Id` int(11) NOT NULL,
   `Name` varchar(128) NOT NULL,
   `Min_pont` int(11) NOT NULL,
   `Max_pont` int(11) NOT NULL,
-  `Description` text NOT NULL,
-  PRIMARY KEY (`Id`)
+  `Description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
@@ -163,8 +158,8 @@ INSERT INTO `ranks` (`Id`, `Name`, `Min_pont`, `Max_pont`, `Description`) VALUES
 -- Tábla szerkezet ehhez a táblához `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
   `Username` varchar(128) NOT NULL,
   `HASH` varchar(128) NOT NULL,
   `Email` varchar(320) NOT NULL,
@@ -174,26 +169,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `Point` int(11) NOT NULL COMMENT 'Pontszám',
   `PermissionId` int(11) NOT NULL COMMENT 'Felhasználói szint',
   `LastLogin` datetime NOT NULL,
-  `ValidatedKey` text NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Username` (`Username`),
-  KEY `RankId` (`RankId`),
-  KEY `PermissionId` (`PermissionId`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `ValidatedKey` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `user`
 --
 
 INSERT INTO `user` (`Id`, `Username`, `HASH`, `Email`, `ActiveUser`, `RankId`, `RegistrationDate`, `Point`, `PermissionId`, `LastLogin`, `ValidatedKey`) VALUES
-(16, 'bazsi', '$2a$04$Q0C3ffL38mWpwMz1okBehOJxe53Lr8r2dWjyKDJm71av6Ems.43iC', 'jb@gmail.com', 1, 1, '2024-03-18', 0, 3, '2024-04-15 08:06:15', ''),
-(17, 'balint', '$2a$04$/quw9t7VqhWaO.tks5Q2L.q3JIFmehNUj5TAbZ6He/aOBoDTfNBeW', 'string', 0, 1, '2024-03-18', 0, 3, '0001-01-01 00:00:00', ''),
-(18, 'balintUser', '$2a$04$echo9khsZ8hNJWAITgbcpuUQSxGHdP50U16x1IobDHB2kAi8h6C9O', 'string@gmail.com', 0, 1, '2024-03-18', 0, 1, '2024-03-25 15:35:15', ''),
+(16, 'bazsi', '$2a$04$Q0C3ffL38mWpwMz1okBehOJxe53Lr8r2dWjyKDJm71av6Ems.43iC', 'jb@gmail.com', 1, 1, '2024-03-18', 0, 3, '2024-04-16 12:07:00', ''),
+(17, 'balint', '$2a$04$/quw9t7VqhWaO.tks5Q2L.q3JIFmehNUj5TAbZ6He/aOBoDTfNBeW', 'string', 1, 1, '2024-03-18', 0, 3, '0001-01-01 00:00:00', ''),
+(18, 'balintUser', '$2a$04$echo9khsZ8hNJWAITgbcpuUQSxGHdP50U16x1IobDHB2kAi8h6C9O', 'string@gmail.com', 1, 1, '2024-03-18', 0, 1, '2024-03-25 15:35:15', ''),
 (19, 'bazsiUser', '$2a$11$CjA9IpkMvxYs9hgjHr4Mk.O.TuW6CvXQXwAlMIBWBFVSfXK3DWqBq', 'juhaszbazsi13@gmail.com', 1, 1, '2024-03-18', 0, 1, '2024-04-09 08:06:45', ''),
-(21, 'valami', '$2a$04$I5xhS9IIXDeLgWqUXKTxhuObH1PA7rW453bFqik75FYyIXGESU0he', 'valami@gmail.com', 1, 1, '2024-04-03', 0, 1, '2024-04-03 11:31:30', ''),
+(21, 'stringasd', '$2a$04$I5xhS9IIXDeLgWqUXKTxhuObH1PA7rW453bFqik75FYyIXGESU0he', 'stringasd@gmail.com', 1, 1, '2024-04-16', 0, 1, '2024-04-03 11:31:30', ''),
 (27, 'viktor', '$2a$04$or979DyWbZAUeiC2T9lRW.r1XU.TRmBJd9BdIvyevvMq9MHK5z89u', 'tviktor20000717@gmail.com', 1, 1, '2024-04-11', 0, 2, '2024-04-15 08:13:43', ''),
-(28, 'adam', '$2a$04$FL.fQGy/HOLmlZurcZmJtuwSqGusnZNo1ZRwJuf5boEbBc9VCoXB2', 'juhaszbazsi13@gmail.com', 0, 1, '2024-04-15', 0, 1, '0001-01-01 00:00:00', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwIiwidXNlcm5hbWUiOiJhZGFtIiwibmJmIjoxNzEzMTY2MDI3LCJleHAiOjE3MTMyNTI0MjcsImlhdCI6MTcxMzE2NjAyNywiaXNzIjoiYXV0aC1hcGkiLCJhdWQiOiJhdXRoLWNsaWVudCJ9.hceUTyxQyoz3rYueSmJ9D-qt8PtakCGTVctkyjQDVAI'),
-(36, 'jacko', '$2a$04$brzNpsopDGP3dN08eXfhGeDBev97pBP5MuMeUn7MejhNHfuTATllC', 'juhaszbazsi13@gmail.com', 1, 1, '2024-04-15', 0, 1, '0001-01-01 00:00:00', '');
+(40, 'jacko', '$2a$04$1Z/ohdz6DvuQRWfCpW1tme.rJFbayDPPIO.rNc1nFfyTVRNhGTePS', 'juhaszb@kkszki.hu', 1, 1, '2024-04-17', 0, 1, '2024-04-17 09:05:46', '');
 
 -- --------------------------------------------------------
 
@@ -201,19 +191,15 @@ INSERT INTO `user` (`Id`, `Username`, `HASH`, `Email`, `ActiveUser`, `RankId`, `
 -- Tábla szerkezet ehhez a táblához `user_post`
 --
 
-CREATE TABLE IF NOT EXISTS `user_post` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_post` (
+  `Id` int(11) NOT NULL,
   `ImageId` int(11) DEFAULT NULL,
   `Description` text NOT NULL,
   `Title` varchar(128) NOT NULL,
   `Like` bigint(20) NOT NULL DEFAULT 0,
   `UserId` int(11) DEFAULT NULL,
-  `uploadDate` date DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `ImageId_2` (`ImageId`,`UserId`),
-  KEY `ImageId` (`ImageId`),
-  KEY `UserId` (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `uploadDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `user_post`
@@ -228,12 +214,10 @@ INSERT INTO `user_post` (`Id`, `ImageId`, `Description`, `Title`, `Like`, `UserI
 -- Tábla szerkezet ehhez a táblához `user_suspicious`
 --
 
-CREATE TABLE IF NOT EXISTS `user_suspicious` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `UserId` (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `user_suspicious` (
+  `Id` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- A tábla adatainak kiíratása `user_suspicious`
@@ -248,14 +232,12 @@ INSERT INTO `user_suspicious` (`Id`, `UserId`) VALUES
 -- Tábla szerkezet ehhez a táblához `user_token`
 --
 
-CREATE TABLE IF NOT EXISTS `user_token` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_token` (
+  `Id` int(11) NOT NULL,
   `UserId` int(11) NOT NULL,
   `Token` text NOT NULL,
-  `Token_expire_date` datetime NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `UserId` (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `Token_expire_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `user_token`
@@ -277,7 +259,146 @@ INSERT INTO `user_token` (`Id`, `UserId`, `Token`, `Token_expire_date`) VALUES
 (13, 16, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYmF6c2kiLCJyb2xlIjoiQWRtaW4iLCJpZCI6IjE2IiwiZXhwIjoxNzEyNzI5MzQyLCJpc3MiOiJhdXRoLWFwaSIsImF1ZCI6ImF1dGgtY2xpZW50In0.WwdT4BXpzUrPZFkoR1rK6jFlB92tRIa8N2kV_ckbwyp2FXQRbBGUJ_j7j4RRL5gmKZxM_TN-Z17qLFYDFPQi5w', '2024-04-10 08:09:02'),
 (14, 16, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYmF6c2kiLCJyb2xlIjoiQWRtaW4iLCJpZCI6IjE2IiwiZXhwIjoxNzEyOTA3ODQ0LCJpc3MiOiJhdXRoLWFwaSIsImF1ZCI6ImF1dGgtY2xpZW50In0.FLt8Fp5l5SXHxkR8TeJUDzau2bh1iuZMK9uedWF6IToodc-MOTLOy-BOjdmsFgxfv0X2_2Cq_EWkjCS7uvo-MQ', '2024-04-12 09:44:04'),
 (15, 16, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYmF6c2kiLCJyb2xlIjoiQWRtaW4iLCJpZCI6IjE2IiwiZXhwIjoxNzEzMjQ3NTc1LCJpc3MiOiJhdXRoLWFwaSIsImF1ZCI6ImF1dGgtY2xpZW50In0.Oy8pixIxtSs1xH-Digo6cgq5eXBQxGDT2zpb3p-q5TQIUjW_7QelvyOrpGhESP0s5EGjbgqahIzvM17HpxH_aw', '2024-04-16 08:06:15'),
-(16, 27, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoidmlrdG9yIiwicm9sZSI6Ik1vZGVyYXRvciIsImlkIjoiMjciLCJleHAiOjE3MTMyNDgwMjMsImlzcyI6ImF1dGgtYXBpIiwiYXVkIjoiYXV0aC1jbGllbnQifQ.mFoVsPRmgVoJqZQkuZMM4AuGA2T29kQTWGMFsBvqb9r9IAHSkjp7TJKVsVj5E02LfKO9YmtabYPTDvZmOTdznA', '2024-04-16 08:13:43');
+(16, 27, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoidmlrdG9yIiwicm9sZSI6Ik1vZGVyYXRvciIsImlkIjoiMjciLCJleHAiOjE3MTMyNDgwMjMsImlzcyI6ImF1dGgtYXBpIiwiYXVkIjoiYXV0aC1jbGllbnQifQ.mFoVsPRmgVoJqZQkuZMM4AuGA2T29kQTWGMFsBvqb9r9IAHSkjp7TJKVsVj5E02LfKO9YmtabYPTDvZmOTdznA', '2024-04-16 08:13:43'),
+(18, 16, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYmF6c2kiLCJyb2xlIjoiQWRtaW4iLCJpZCI6IjE2IiwiZXhwIjoxNzEzMzM3MDAyLCJpc3MiOiJhdXRoLWFwaSIsImF1ZCI6ImF1dGgtY2xpZW50In0.9z_KlDOj12aMPjG-xmkdryVH7T4lvVeUwEFigS9t-Moi7gY2XX4ty_Zh8x3KKMo9jTPOkDAe3Tk49f7ZLeozdg', '2024-04-17 08:56:42'),
+(19, 16, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiYmF6c2kiLCJyb2xlIjoiQWRtaW4iLCJpZCI6IjE2IiwiZXhwIjoxNzEzMzQ4NDIwLCJpc3MiOiJhdXRoLWFwaSIsImF1ZCI6ImF1dGgtY2xpZW50In0.gcAL4O-KwFo09bFY-YhorkwLWnMVKxZZW4mu2gbsC4rIGci-1B1Z9prd_l8jEvgaiw0bK0XgxGt9rYcodXXO4g', '2024-04-17 12:07:00'),
+(20, 40, 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiamFja28iLCJyb2xlIjoiRGVmYXVsdCIsImlkIjoiNDAiLCJleHAiOjE3MTM0MjM5NDYsImlzcyI6ImF1dGgtYXBpIiwiYXVkIjoiYXV0aC1jbGllbnQifQ.hIqUAyEIl09KatJZv-oPWEunzHjchVtH3lY8k4U5yMKnFwF1kP4TS-wckkR-chWujNjM4tGq4k49OXgZ5vVzQA', '2024-04-18 09:05:46');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `alertmessage`
+--
+ALTER TABLE `alertmessage`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- A tábla indexei `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `CommentId` (`CommentId`),
+  ADD KEY `UserId` (`UserId`),
+  ADD KEY `PostId` (`PostId`);
+
+--
+-- A tábla indexei `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `liked_posts`
+--
+ALTER TABLE `liked_posts`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `PostID` (`PostID`);
+
+--
+-- A tábla indexei `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `ranks`
+--
+ALTER TABLE `ranks`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD KEY `RankId` (`RankId`),
+  ADD KEY `PermissionId` (`PermissionId`);
+
+--
+-- A tábla indexei `user_post`
+--
+ALTER TABLE `user_post`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `ImageId_2` (`ImageId`,`UserId`),
+  ADD KEY `ImageId` (`ImageId`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- A tábla indexei `user_suspicious`
+--
+ALTER TABLE `user_suspicious`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `UserId` (`UserId`);
+
+--
+-- A tábla indexei `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `alertmessage`
+--
+ALTER TABLE `alertmessage`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT a táblához `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT a táblához `images`
+--
+ALTER TABLE `images`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT a táblához `liked_posts`
+--
+ALTER TABLE `liked_posts`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT a táblához `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT a táblához `user_post`
+--
+ALTER TABLE `user_post`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT a táblához `user_suspicious`
+--
+ALTER TABLE `user_suspicious`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT a táblához `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -293,15 +414,15 @@ ALTER TABLE `alertmessage`
 -- Megkötések a táblához `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`PostId`) REFERENCES `user_post` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`PostId`) REFERENCES `user_post` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Megkötések a táblához `liked_posts`
 --
 ALTER TABLE `liked_posts`
-  ADD CONSTRAINT `liked_posts_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`Id`),
-  ADD CONSTRAINT `liked_posts_ibfk_2` FOREIGN KEY (`PostID`) REFERENCES `user_post` (`Id`);
+  ADD CONSTRAINT `liked_posts_ibfk_2` FOREIGN KEY (`PostID`) REFERENCES `user_post` (`Id`),
+  ADD CONSTRAINT `liked_posts_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Megkötések a táblához `user`
@@ -328,17 +449,6 @@ ALTER TABLE `user_suspicious`
 --
 ALTER TABLE `user_token`
   ADD CONSTRAINT `user_token_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-DELIMITER $$
---
--- Események
---
-CREATE DEFINER=`root`@`localhost` EVENT `token_delete` ON SCHEDULE EVERY 1 HOUR STARTS '2024-03-17 19:33:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM user_token
-WHERE user_token.token_expire_date < NOW()$$
-
-CREATE DEFINER=`root`@`localhost` EVENT `deleteExpiredTokens` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-02-19 08:42:52' ON COMPLETION NOT PRESERVE DISABLE COMMENT 'Clears out sessions table each hour.' DO DELETE FROM user_token WHERE user_token.token_expire_date < NOW()$$
-
-DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
