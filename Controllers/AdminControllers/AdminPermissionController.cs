b5ce1e92e4af28366c12b349a4737c40977a1854
juhaszb1 <juhaszb@kkszki.hu>
2024-04-29 20:47:05 +0200

    Add AdminPermissionController, add allpermission endpoint

---
+++ Controllers/AdminControllers/AdminPermissionController.cs
@@ -0,0 +1,30 @@
+﻿using Connect2Gether_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connect2Gether_API.Controllers.AdminControllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminPermissionController : ControllerBase
+    {
+        [HttpGet("AllPermission")]
+        public IActionResult AllPermission()
+        {
+            using (var context = new Connect2getherContext())
+            {
+                try
+                {
+                    var permissions = context.Permissions.ToList();
+                    return Ok(permissions);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
